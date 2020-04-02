ffd57b8e587c3688a44fefcd8bb12a52a4a966b0
= Casey Law <= caseyjlaw@gmail.com>
2020-04-02 13:05:57 -0700

    using versioning with git

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
+from version import get_git_version
 
 setup(name='dsa110-T2',
-      version='0.1',
+      version=get_git_version(),
       url='http://github.com/dsa110/dsa110-T2',
       packages=['T2'],
       zip_safe=False)
