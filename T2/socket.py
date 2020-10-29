250988cb432191e089a282653c84e2fad80f01f3
Casey Law <caseyjlaw@gmail.com>
2020-10-29 15:32:26 -0700

    cleanup of socket and run_T2 script

--- T2/socket.py
+++ T2/socket.py
@@ -8,7 +8,7 @@ logger.subsystem('software')
 logger.app('T2')
 
 
-def parse_socket(host, port, selectcols=['itime', 'idm', 'ibox', 'ibeam']):
+def parse_socket(host, port, selectcols=['itime', 'idm', 'ibox', 'ibeam'], outputfile=None, plot_dir=None):
     """ 
     Takes standard MBHeimdall giants socket output and returns full table, classifier inputs and snr tables.
     selectcols will take a subset of the standard MBHeimdall output for cluster. 
@@ -16,40 +16,7 @@ def parse_socket(host, port, selectcols=['itime', 'idm', 'ibox', 'ibeam']):
     host, port: same with heimdall -coincidencer host:port 
     selectcol: list of str.  Select columns for clustering. 
     """
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    s.bind((host,port))    # assigns the socket with an address
-    s.listen(5)             # accept no. of incoming connections
-    
-    gulp_i = 0 # track heimdall gulp number 
-    while True:
-        clientsocket, address = s.accept() # stores the socket details in 2 variables
-        logger.info(f"Connection from {address} has been established")
-        gulp_i += 1 
-        logger.info(f"gulp {gulp_i}")
-        
-        # read in heimdall socket output  
-        ascii_letter = clientsocket.recv(1)           # recieves an alphabet whose ASCII value is the size of the message 
-        
-        if len(ascii_letter) > 0:
-            size = ord(ascii_letter.decode('utf-8'))      # ord() returns the ASCII value of a character
-            #candsfile = clientsocket.recv(size)           # recieving the actual msg        
-            candsfile = clientsocket.recv(10000000).decode('utf-8')  # python 3 requires argument here. how to make sure the size is big enough? 
-            clientsocket.close()   
-            
-            # do we want both heimdall.cand and giants.out?  From two sockets?  
-            # reading in one gulp at a time. 
-            # will modify to continue reading in.  
-            tab, data, snrs = cluster_heimdall.parse_candsfile(candsfile)
-            
-            return tab, data, snrs
-        else: 
-            logger.info("This gulp has no heimdall giants output.")
 
-
-def parse_socket_and_cluster_and_plot(host="127.0.0.1", port=12345, selectcols=['itime', 'idm', 'ibox', 'ibeam'], outputfile="T2_output_socket_", plot=False, plot_dir="socket_"):
-    """ 
-    continuously read in socket, cluster, dump results to outputfile and save plots.
-    """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     s.bind((host,port))    # assigns the socket with an address
     s.listen(5)             # accept no. of incoming connections
@@ -66,28 +33,39 @@ def parse_socket_and_cluster_and_plot(host="127.0.0.1", port=12345, selectcols=[
         logger.info(f"Connection from {address} has been established")
         gulp_i += 1 
         logger.info(f"gulp {gulp_i}")
-        
+
         # read in heimdall socket output  
-        candsfile = clientsocket.recv(100000000)  # python 3 requires argument here. how to make sure the size is big enough?         
+        ascii_letter = clientsocket.recv(1)           # recieves an alphabet whose ASCII value is the size of the message 
         
-        if len(candsfile) == 0:
+        if len(ascii_letter) == 0:
             logger.info("This gulp has no heimdall giants output.")
-        
         else: 
             logger.info("Reading candsfile...")
-            candsfile = candsfile.decode('utf-8')               # decode the bytes msg 
+            candsfile = clientsocket.recv(10000000).decode('utf-8')
             clientsocket.close()   
             tab, data, snrs = cluster_heimdall.parse_candsfile(candsfile)
             logger.info(f"Table has {len(tab)} rows")
-            
-            # T2 cluster 
-            clusterer, data_labeled = cluster_heimdall.cluster_data(data, min_cluster_size=10, min_samples=10, metric='euclidean', allow_single_cluster=True, return_clusterer=True)
-            clsnr = cluster_heimdall.get_peak(data_labeled, snrs) 
-            clsnr = cluster_heimdall.filter_clustered(clsnr)
+            try:
+                cluster_and_plot(tab, data, snrs, gulp_i, selectcols=selectcols, outputfile=outputfile, plot_dir=plot_dir)
+            except KeyboardInterrupt:
+                logger.info("Escaping plotting")
+                break
 
-            # send T2 cluster results to outputfile
-            cluster_heimdall.dump_cluster_results(tab, clsnr, outputfile+str(gulp_i)+".txt", output_cols=['mjds', 'snr', 'ibox', 'dm'])
+
+def cluster_and_plot(tab, data, snrs, gulp_i, selectcols=['itime', 'idm', 'ibox', 'ibeam'], outputfile=None, plot_dir=None):
+    """ 
+    Run clustering and plotting on read data.
+    """
+
+    # cluster 
+    clusterer, data_labeled = cluster_heimdall.cluster_data(data, min_cluster_size=10, min_samples=10, metric='euclidean', allow_single_cluster=True, return_clusterer=True)
+    clsnr = cluster_heimdall.get_peak(data_labeled, snrs) 
+    clsnr = cluster_heimdall.filter_clustered(clsnr)
+
+    # send T2 cluster results to outputfile
+    if outputfile is not None:
+        cluster_heimdall.dump_cluster_results(tab, clsnr, outputfile+str(gulp_i)+".txt", output_cols=['mjds', 'snr', 'ibox', 'dm'])
             
-            if plot: 
-                plotting.plot_giants(tab, plot_dir=plot_dir+str(gulp_i)+"_") # plot giants      
-                plotting.plot_clustered(clusterer, clsnr, snrs, data, tab, cols=['itime', 'idm', 'ibox'], plot_dir=plot_dir+str(gulp_i)+"_") # plot cluster results  
+    if plot_dir is not None: 
+         plotting.plot_giants(tab, plot_dir=plot_dir+str(gulp_i)+"_") # plot giants      
+         plotting.plot_clustered(clusterer, clsnr, snrs, data, tab, cols=['itime', 'idm', 'ibox'], plot_dir=plot_dir+str(gulp_i)+"_") # plot cluster results  
