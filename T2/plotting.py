998faf193d907cbf3c1fc6cde071e6527bf49329
Casey Law <caseyjlaw@gmail.com>
2020-11-04 11:26:39 -0800

    using Agg backend

--- T2/plotting.py
+++ T2/plotting.py
@@ -1,5 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import seaborn as sns
 import dsautils.dsa_syslog as dsl
