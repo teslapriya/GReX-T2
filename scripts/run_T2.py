fc0db7b12a29d5df9e0dd5ce2513bd30953c6449
Casey Law <caseyjlaw@gmail.com>
2020-11-05 15:51:08 -0800

    using argparse. not working yet

--- scripts/run_T2.py
+++ scripts/run_T2.py
@@ -2,39 +2,17 @@
 # -*- coding: utf-8 -*-
 # dsahead python 3.7
 import T2 
-import sys, getopt  
+import sys
 import argparse
 
 
 def main(argv):
-    """    
-    Use getopt.getopt to parse command line options.
-    Parameters
-    ----------
-    argv : TYPE
-        DESCRIPTION.
-    """
-    inputfile = ''
-    outputfile = ''
-    plotdir = '' 
-    try:
-       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile=", "plotdir="])
-    except getopt.GetoptError:
-       print('run_T2.py -i <inputfile> -o <outputfile> -p <saveplotdir>')
-       sys.exit(2)
-    for opt, arg in opts:
-       if opt == '-h':
-          print('run_T2.py -i <inputfile> -o <outputfile> -p <plotdir>')
-          sys.exit()
-       elif opt in ("-i", "--ifile"):
-          inputfile = arg
-       elif opt in ("-o", "--ofile"):
-          outputfile = arg
-       elif opt in ("-p", "--plotdir"):
-          outputfile = arg
-    print('Input file is "', inputfile)
-    print('Output file is "', outputfile)
-    print('Save plots at "', plotdir) 
+    parser = argparse.ArgumentParser(description='Parse input to T2 socket clients')
+    parser.add_argument('ip', type=str, help='ip address of heimdall')
+    parser.add_argument('ports', type=str, help='ports address of heimdall')
+
+    args = parser.parse_args()
+    print(args)
 
 if __name__ == '__main__':
     T2.socket.parse_socket(host="10.41.0.109", ports=[12345], selectcols=['itime', 'idm', 'ibox', 'ibeam'], outputfile="cluster_output", plot_dir=None)
