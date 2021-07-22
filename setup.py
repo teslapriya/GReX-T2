deaf124c2980948e3e7f6e114c53694b19d14cce
Casey Law <caseyjlaw@gmail.com>
2021-07-22 14:59:07 -0700

    adding requirements

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from version import get_git_version
 setup(name='dsa110-T2',
       version=get_git_version(),
       url='http://github.com/dsa110/dsa110-T2',
+      requirements=['seaborn', 'astropy', 'hdbscan'],
       packages=['T2'],
       zip_safe=False)
