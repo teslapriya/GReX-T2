2900a39c551f08c96de70e992502ac79cf37d85a
Kiran Shila <me@kiranshila.com>
2024-06-10 18:28:14 -0700

    Back to INFO logging

--- scripts/run_socket_grex.py
+++ scripts/run_socket_grex.py
@@ -10,7 +10,7 @@ PORT = 12345
 # TODO Write to OpenTelemetry as well to collect logs for grafana
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="[%(asctime)s] [%(levelname)s] %(message)s",
     handlers=[logging.FileHandler("output.log"), logging.StreamHandler()],
 )
